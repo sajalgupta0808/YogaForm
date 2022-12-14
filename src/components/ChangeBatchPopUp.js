14643166b80c9d6e2255ec54bad7dd3d4bc791c1
Gupta Sajal <guptasajal@Sajals-MacBook-Air.local>
2022-12-14 19:49:55 +0530

    eighth commit

---
+++ src/components/ChangeBatchPopUp.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
+export default function ChangeBatchPopUp() {
+    return ( <
+        >
+        <
+        div className = "modal show"
+        style = {
+            { display: 'block', position: 'initial' } } >
+        <
+        Modal.Dialog >
+        <
+        Modal.Header closeButton >
+        <
+        Modal.Title > Modal title < /Modal.Title> <
+        /Modal.Header>
+
+        <
+        Modal.Body >
+        <
+        p > Modal body text goes here. < /p> <
+        /Modal.Body>
+
+        <
+        Modal.Footer >
+        <
+        Button variant = "secondary" > Close < /Button> <
+        Button variant = "primary" > Save changes < /Button> <
+        /Modal.Footer> <
+        /Modal.Dialog> <
+        /div> <
+        />
+
+
+
+    )
+}
